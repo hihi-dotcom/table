5329743d1d682253fc01e2467bc9a89baddd5f4f
Adam Czirjak <124435921+hihi-dotcom@users.noreply.github.com>
2024-10-15 20:19:34 +0200

    JS function building

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const array = [
+let array = [
     
         {
             firstname1: 'Géza',
@@ -31,92 +31,98 @@ const array = [
 
 
 
-const table = document.createElement('table');
- document.body.appendChild(table);
-const thead = document.createElement('thead');
-const theadrow = document.createElement('tr');
-const th1 = document.createElement('th'); //vezeteknev
-const th2 = document.createElement('th');//keresztnev
-const th3 = document.createElement('th');//married
-const th4 = document.createElement('th');//pet
-
-table.appendChild(thead);
-thead.appendChild(theadrow);
-theadrow.appendChild(th1);
-theadrow.appendChild(th2);
-theadrow.appendChild(th3);
-theadrow.appendChild(th4);
-th2.colSpan= 2;                 
-th1.innerHTML = "Vezetéknév";
-th2.innerHTML = "Keresztnév";
-th3.innerHTML = "Házas-e";
-th4.innerHTML = "Háziállat";
+    const table = document.createElement('table');
+    document.body.appendChild(table);
+   const thead = document.createElement('thead');
+   const theadrow = document.createElement('tr');
+   const th1 = document.createElement('th'); //vezeteknev
+   const th2 = document.createElement('th');//keresztnev
+   const th3 = document.createElement('th');//married
+   const th4 = document.createElement('th');//pet
 
 
-const tbody = document.createElement('tbody');
-table.appendChild(tbody);
 
-
-for(const pers of array){
-    const tr = document.createElement('tr');
-    const td = document.createElement('td'); // lastname
-    const td2 = document.createElement('td'); //firstname1
-    const td3 = document.createElement('td'); // lastname
-    const td4 = document.createElement('td'); //firstname1
-   const td5 = document.createElement('td');
-
-    tbody.appendChild(tr);
-    tr.appendChild(td);
-    tr.appendChild(td2);
+   table.appendChild(thead);
+   thead.appendChild(theadrow);
+   theadrow.appendChild(th1);
+   theadrow.appendChild(th2);
+   theadrow.appendChild(th3);
+   theadrow.appendChild(th4);
+   th2.colSpan= 2;                 
+   th1.innerHTML = "Vezetéknév";
+   th2.innerHTML = "Keresztnév";
+   th3.innerHTML = "Házas-e";
+   th4.innerHTML = "Háziállat";
+   const tbody = document.createElement('tbody');
+    table.appendChild(tbody);
     
+   
+   emberhozzaadas();
+   
+function emberhozzaadas(array){
     
-    tr.addEventListener('click', (e) => {
-        console.log('clicked');
-      
-        const kiv = tbody.querySelector('.selected');
-        if(kiv != undefined){
-            kiv.classList.remove('selected');
+    for(const pers of array){
+        const tr = document.createElement('tr');
+        const td = document.createElement('td'); // lastname
+        const td2 = document.createElement('td'); //firstname1
+        const td3 = document.createElement('td'); // lastname
+        const td4 = document.createElement('td'); //firstname1
+       const td5 = document.createElement('td');
+   
+             tbody.appendChild(tr);
+             tr.appendChild(td);
+             tr.appendChild(td2);
+         };       
+             
+          /*  tr.addEventListener('click', (e) => {
+                 console.log('clicked');
+                 
+                 const kiv = tbody.querySelector('.selected');
+                 if(kiv != undefined){
+                     kiv.classList.remove('selected');
+                 }
+                 e.currentTarget.classList.add('selected');
+             });*/
+         
+             td.innerHTML = pers.lastname;
+             td2.innerHTML = pers.firstname1;
+             
+             td4.innerHTML = pers.married;
+             td5.innerHTML = pers.pet;
+             
+             if(pers.married === true){
+                 td4.innerHTML = "igen";
+             }
+             else{
+                 td4.innerHTML = "nem";
+             }
+             
+};         
+         if(pers.firstname2 === undefined){
+             td2.colSpan = 2;
+         }
+         else{
+             const td3 = document.createElement('td');//firstname2
+             tr.appendChild(td3);
+             td3.innerHTML = pers.firstname2;
         }
-        e.currentTarget.classList.add('selected');
-    });
+        tr.appendChild(td4);
+        tr.appendChild(td5);
+    
+    
 
-    td.innerHTML = pers.lastname;
-    td2.innerHTML = pers.firstname1;
+
    
-    td4.innerHTML = pers.married;
-    td5.innerHTML = pers.pet;
+   const form = document.getElementById('form');
+   form.addEventListener('submit', (e) => {
+       const lastname = document.getElementById('lastname');
+       const firstname1 = document.getElementById('firstname1');
+       const firstname2 = document.getElementById('firstname2');
+       const married = document.getElementById('married');
    
-    if(pers.married === true){
-        td4.innerHTML = "igen";
-    }
-    else{
-        td4.innerHTML = "nem";
-    }
+       const lastnamevalue = lastname.value;
+       const firstname1value = firstname1.value;
+       const firstname2value = firstname2.value;
+       const marriedvalue = married.checked;
    
-
-if(pers.firstname2 === undefined){
-    td2.colSpan = 2;
-}
-else{
-    const td3 = document.createElement('td');//firstname2
-    tr.appendChild(td3);
-    td3.innerHTML = pers.firstname2;
-}
-    tr.appendChild(td4);
-    tr.appendChild(td5);
-};
-
-
-const form = document.getElementById('form');
-form.addEventListener('submit', (e) => {
-    const lastname = document.getElementById('lastname');
-    const firstname1 = document.getElementById('firstname1');
-    const firstname2 = document.getElementById('firstname2');
-    const married = document.getElementById('married');
-
-    const lastnamevalue = lastname.value;
-    const firstname1value = firstname1.value;
-    const firstname2value = firstname2.value;
-    const marriedvalue = married.checked;
-
-});
+   });
